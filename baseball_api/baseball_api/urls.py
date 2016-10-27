c4b336456653a7bf1af0d20458461a4aa2f47c0e
destanton <delisestanton@gmail.com>
2016-10-26 22:55:27 -0400

    adding put and delete views

--- baseball_api/baseball_api/urls.py
+++ baseball_api/baseball_api/urls.py
@@ -16,12 +16,19 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from baseball_data.views import MasterListCreateAPIView, BattingListCreateAPIView, FieldingListCreateAPIView,\
-                                PitchingListCreateAPIView
+                                PitchingListCreateAPIView, MasterDetailUpdateDestroyAPIView, BattingDetailUpdateDestroyAPIView,\
+                                FieldingDetailUpdateDestroyAPIView, PitchingDetailUpdateDestroyAPIView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/masters/$', MasterListCreateAPIView.as_view(), name="master_list_create_api_view"),
     url(r'^api/battings/$', BattingListCreateAPIView.as_view(), name="batting_list_create_api_view"),
     url(r'^api/fieldings/$', FieldingListCreateAPIView.as_view(), name="fielding_list_create_api_view"),
-    url(r'^api/pitchings/$', PitchingListCreateAPIView.as_view(), name="pitching_list_create_api_view")
+    url(r'^api/pitchings/$', PitchingListCreateAPIView.as_view(), name="pitching_list_create_api_view"),
+    url(r'^api/masters/(?P<pk>\d+)/$', MasterDetailUpdateDestroyAPIView.as_view(), name="master_detail_update_destroy_api_view"),
+    url(r'^api/battings/(?P<pk>\d+)/$', BattingDetailUpdateDestroyAPIView.as_view(), name="batting_detail_update_destroy_api_view"),
+    url(r'^api/fieldings/(?P<pk>\d+)/$', FieldingDetailUpdateDestroyAPIView.as_view(), name="fielding_detail_update_destroy_api_view"),
+    url(r'^api/pitchings/(?P<pk>\d+)/$', PitchingDetailUpdateDestroyAPIView.as_view(), name="pitching_detail_update_destroy_api_view"),
 ]
+#
+# localhost:8000/api/:resource_name/:pk/
--- baseball_api/baseball_data/views.py
+++ baseball_api/baseball_data/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from baseball_data.models import Master, Batting, Fielding, Pitching
 from baseball_data.serializers import MasterSerializer, BattingSerializer, FieldingSerializer, PitchingSerializer
 
@@ -20,5 +20,25 @@ class FieldingListCreateAPIView(ListCreateAPIView):
 
 
 class PitchingListCreateAPIView(ListCreateAPIView):
-    quesryset = Pitching.objects.all()
+    queryset = Pitching.objects.all()
+    serializer_class = PitchingSerializer
+
+
+class MasterDetailUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Master.objects.all()
+    serializer_class = MasterSerializer
+
+
+class BattingDetailUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Batting.objects.all()
+    serializer_class = BattingSerializer
+
+
+class FieldingDetailUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Pitching.objects.all()
+    serializer_class = FieldingSerializer
+
+
+class PitchingDetailUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Pitching.objects.all()
     serializer_class = PitchingSerializer
